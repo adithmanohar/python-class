5a3ff2f260d66d4997245db4808a6815cee8f3a3
adithmanohar <adithmanohar@gmail.com>
2026-02-09 10:34:22 +0530

    updates task3 file

--- task3.py
+++ task3.py
@@ -2,15 +2,15 @@ a=int(input("enter a number"))
 b=input("enter a operator + - / * ")
 c=int(input("enter a number"))
 if b=="+":
-    print(a+c)
+    print(f"result {a+c}")
 elif b=="-":
-    print(a-c)
+    print(f"result {a-c}")
 elif b=="*":
-    print(a*c)
+    print(f"result {a*c}")
     
 elif b=="/":
     if   c!=0:
-        print(a/c)     
+        print(f"result {a/c}")     
     else:
         print("cannot divided by zero")
 else:
